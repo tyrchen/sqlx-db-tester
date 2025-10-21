0d4ffdcf80c05b6c4b96a6b5d1da4665d2570710
Tyr Chen <tyr.chen@gmail.com>
2025-10-20 20:42:49 -0700

    chore: add features to exclude mysql

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+#![cfg(feature = "mysql")]
 mod mysql;
+#[cfg(feature = "postgres")]
 mod postgres;
 
+#[cfg(feature = "mysql")]
 pub use mysql::TestMySql;
+#[cfg(feature = "postgres")]
 pub use postgres::TestPg;
